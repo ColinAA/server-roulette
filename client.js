8286150678d0a2a23c1f1361f0efd9c905a937bf
Omri Bernstein <hippiccolo@gmail.com>
2016-11-30 11:33:20 -0500

    Fully working undeployed version

---
+++ client.js
@@ -0,0 +1,55 @@
+const usernameInputElem = document.getElementById('username-input');
+const highScoreElem = document.getElementById('high-score');
+const rouletteButton = document.getElementById('roulette');
+const resultElem = document.getElementById('result');
+const currentScoreElem = document.getElementById('current-score');
+const gameElem = document.getElementById('game');
+
+let currentScore = 0;
+function submitClick () {
+  const body = JSON.stringify({
+    username: usernameInputElem.value
+  });
+  var headers = new Headers({
+    'Content-Type': 'application/json'
+  });
+  return fetch('/api/click', {
+    method: 'POST',
+    body,
+    headers
+  })
+  .then(function (response) {
+    if (response.status === 500) {
+      resultElem.innerText = 'You crashed the server!';
+      currentScore = 0;
+
+    } else {    
+      resultElem.innerText = 'Success';
+      currentScore++;
+    }
+    currentScoreElem.innerText = currentScore;
+  });
+}
+
+function updateHighScore () {
+  return fetch(`/api/highScore?username=${usernameInputElem.value}`)
+  .then(response => response.json())
+  .then(function (data) {
+    highScoreElem.innerText = data.score;
+    return data.score;
+  });
+}
+
+rouletteButton.onclick = function () {
+  submitClick()
+  .then(updateHighScore);
+};
+
+usernameInputElem.onkeyup = function () {
+  if (usernameInputElem.value.length === 0) {
+    gameElem.style.visibility = 'hidden';
+  } else {
+    gameElem.style.visibility = 'visible';
+    updateHighScore();
+  }
+};
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const volleyball = require('volleyball');
+const Sequelize = require('sequelize');
+
+// routes
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(volleyball);
+
+app.use(express.static(__dirname));
+
+app.post('/api/click', function (req, res, next) {
+  const isAboutToCrash = Math.random() > 0.5;
+  ClickRecord.create({
+    didCrash: isAboutToCrash,
+    username: req.body.username
+  })
+  .then(function () {
+    if (isAboutToCrash) {
+      res.status(500).end();
+      process.exit();
+    } else {
+      res.status(201).end();
+    }
+  })
+  .catch(next);
+});
+
+app.get('/api/highScore', function (req, res, next) {
+  ClickRecord.highScoreForUser(req.query.username)
+  .then(function (score) {
+    res.status(200).json({score});
+  })
+  .catch(next);
+});
+
+// models
+const db = new Sequelize('postgres://localhost:5432/server-roulette');
+const ClickRecord = db.define('click_record', {
+  didCrash: Sequelize.BOOLEAN,
+  username: {
+    type: Sequelize.STRING
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+}, {
+  classMethods: {
+    highScoreForUser: function (username) {
+      return this.findAll({
+        where: {username},
+        order: '"createdAt" DESC'
+      })
+      .then(function (clickRecords) {
+        let runningMax = 0;
+        let globalMax = 0;
+        clickRecords.forEach(function (clickRecord) {
+          if (clickRecord.didCrash) {
+            runningMax = 0;
+          } else {
+            runningMax++;
+            globalMax = runningMax > globalMax ? runningMax : globalMax;
+          }
+        });
+        return globalMax;
+      });
+    }
+  }
+});
+
+// startup server
+const port = 3000;
+db.sync()
+.then(function () {
+  return new Promise(function (resolve, reject) {
+    app.listen(port, function (err) {
+      if (err) reject(err);
+      else console.log('Your wish is my command on port', port);
+    });
+  });
+})
+.catch(function (err) {
+  console.error('Problem starting server')
+  console.error(err);
+});
